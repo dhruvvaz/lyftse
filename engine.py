157a291f0abb21181d00d5bf4e8405dca88a8585
Dhruv1010101 <77336102+Dhruv1010101@users.noreply.github.com>
2023-07-05 18:49:55 -0400

    Create engine.py

---
+++ engine.py
@@ -0,0 +1,10 @@
+from car_part import CarPart
+
+class Engine(CarPart):
+    def __init__(self, last_service_date, current_mileage, last_service_mileage):
+        super().__init__(last_service_date)
+        self.current_mileage = current_mileage
+        self.last_service_mileage = last_service_mileage
+
+    def needs_service(self):
+        return self.current_mileage - self.last_service_mileage > 30000
